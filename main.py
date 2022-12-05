ff0aa8cc952c2e5b90b3ca9402a0acfbfc7354a3
Onni_Virtanen <onni.virtanen5@gmail.com>
2022-12-05 22:45:45 +0200

    fixed bugs and added comments

--- main.py
+++ main.py
@@ -3,104 +3,110 @@ from Crypto.Cipher import AES
 
 # Encrypt file
 def encrypt():
+    # Create a random secure key.
+    # Ask user to enter the filename to encrypt.
     key = os.urandom(16)
     filename = input("Enter the filename you want to encrypt: \n")
-
     print(f'Encrypting file "{filename}"...')
-    with open(filename, "rb") as tieto:
-        data = tieto.read()
+
+    # Read bytes of the file.
+    # Encrypt the file's bytes and assign it to a variable ciphertext.
+    # Create a nonce and a tag.
+    with open(filename, "rb") as f:
+        data = f.read()
     cipher = AES.new(key, AES.MODE_EAX)
     nonce = cipher.nonce
     ciphertext, tag = cipher.encrypt_and_digest(data)
+
+    # User must save the printed key for decryption.
     print("The key is required for decryption. If lost, the data will be irrecoverable.")
     print("key:", key.hex())
 
     # Asking user if the key should be saved in the current directory.
+    # If true, creating a file containing the key.
     save_key = input(
         "Write the key to a file in your current directory? Y/N\n")
     if save_key == "Y" or save_key== "y":
         with open(f'key_{filename}.txt', "w") as f:
             f.write("key: " + key.hex())
 
-    # Writing ciphertext to file.
+    # Creating a file that contains the encrypted data.
     with open('enc_' + filename, "wb") as f:
         f.write(ciphertext)
-    # Writing nonce to the file.
+    # Writing nonce to end of the file.
     with open('enc_' + filename, "a") as f:
         f.write(nonce.hex())
-    # Writing tag to the file.
+    # Writing tag to end of the file.
     with open('enc_' + filename, "a") as f:
         f.write(tag.hex())
+    
     print("The file was successfully encrypted.")
-
 
 # Decrypt file
 def decrypt():
     filename = input("Type a filename you want to decrypt: \n")
 
-    # Finding nonce and tag from the encrypted file.
+    # Reading nonce and tag from the file that is to be decrypted.
+    # Assigning variables to nonce and tag.
     with open(filename, "r") as f:
-        # NONCE
+        # Nonce
         f.seek(0, 2)
         position = f.tell()
         f.seek(position - 64, 0)
-        bytes64_32 = f.read(32)
-        nonce_in_end_of_file = bytes64_32
+        nonce = f.read(32)
+        nonce = bytes.fromhex(nonce)
 
-        # TAG
-        bytes32_16 = f.read()
+        # Tag
+        tag= f.read()
         f.seek(position - 32, 0)
-        bytes32_16 = f.read()
-        tag_in_end_of_file = bytes32_16
+        tag = f.read()
+        tag = bytes.fromhex(tag)
 
-        nonce = bytes.fromhex(nonce_in_end_of_file)
-
+    # Remove nonce and tag from end of the file that is to be decrypted.
     with open(filename, 'r+b') as f:
-        # Move to the position 64 bytes from the end of the file
         f.seek(-64, 2)
-        # Truncate the file at the current position
         f.truncate()
 
-    key_input = input(
-        "Type the key that was given when the file was encrypted: \n")
+    # Asking the user for key input.
+    key_input = input("Type the key for decryption: \n")
     key = bytes.fromhex(key_input)
+
+    # Reading bytes of the encrypted file and assigning it to a variable.
     with open(filename, "rb") as f:
         ciphertext_file = f.read()
 
-    tag_file = bytes.fromhex(tag_in_end_of_file)
-
-    cipher = AES.new(key, AES.MODE_EAX, nonce=nonce)
-    plaindata = cipher.decrypt(ciphertext_file)
+    # Decrypting the encrypted file using the key provided by user.
     try:
-        try:
-            cipher.verify(tag_file)
-            print("The file is authentic.")
-        except:
-            print("The file's authenticity cannot be verified.")
+        cipher = AES.new(key, AES.MODE_EAX, nonce=nonce)
+        plaindata = cipher.decrypt(ciphertext_file)
+    except:
+        print("Error in decryption. Key or nonce faulty.")
 
+    # Veryfying the file's authenticity by using the tag.
+    try:
+        cipher.verify(tag)
+        print("The file is authentic.")
+    except:
+        print("The file's authenticity cannot be verified.")
+
+    # Creating a file from the decrypted data.
+    try:
         with open("dec_" + filename[4:], "wb") as f:
             f.write(plaindata)
         print("The file was successfully decrypted.")
-    except ValueError:
-        print("Key incorrect or message corrupted")
+    except:
+        print("Error when writing decrypted data to a file.")
 
-# User input
+# Introduction to the program.
 print("Welcome to ViCrypt!")
 print("This program is used for file encryption and decryption.")
-print("The program uses AES 256 algorithm to encrypt files.")
+print("The program uses AES256 algorithm.")
 
-proceed = str(input("Proceed: Y/N\n"))
-
-if proceed == "Y" or proceed == "y":
-    user_choice_enc_or_dec = input("Type in E or D, E=Encrypt, D=Decrypt.\n")
-
-    if user_choice_enc_or_dec == "E":
-        encrypt()
-    elif user_choice_enc_or_dec == "D":
-        decrypt()
-    else:
-        print("error")
-elif proceed == "N" or proceed == "n":
-    print("Closing program.")
+# Asking the user how the program will be used. It can be used for encryption or decryption.
+user_choice = input("Type in E or D, E=Encrypt, D=Decrypt.\n")
+if user_choice == "E" or user_choice == "e":
+    encrypt()
+elif user_choice == "D" or user_choice == "d":
+    decrypt()
 else:
-    print("Wrong input.\nClosing program.")
+    print("Error in user input when providing choice on whether to encrypt or decrypt.")
