590e40198e65e1f325e242c57578cc4a993a039a
Onni_Virtanen <onni.virtanen5@gmail.com>
2022-12-05 22:10:02 +0200

    add

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-# Program made by Onni Virtanen 05/12/2022.
-
 import os
 from Crypto.Cipher import AES
-
 
 # Encrypt file
 def encrypt():
@@ -15,21 +12,15 @@ def encrypt():
     cipher = AES.new(key, AES.MODE_EAX)
     nonce = cipher.nonce
     ciphertext, tag = cipher.encrypt_and_digest(data)
-    print("The key and nonce are required for decryption. If they are lost, the data will be irrecoverable.")
-    print("The tag is used to verify the authenticity of the file.")
-    print("Please make sure to save these values:\n")
+    print("The key is required for decryption. If lost, the data will be irrecoverable.")
     print("key:", key.hex())
-    print("nonce:", nonce.hex())
-    print("tag:", tag.hex() + "\n")
 
-    # Asking user if it is necessary to write a file containing key, nonce and tag in current directory.
-    save_important_values_to_file = input(
-        "Write these values to a file in your current directory? Y/N\n")
-    if save_important_values_to_file == "Y" or save_important_values_to_file == "y":
-        with open(f'important_values_{filename}.txt', "w") as f:
-            f.write("key: " + key.hex() + "\n")
-            f.write("nonce: " + nonce.hex() + "\n")
-            f.write("tag: " + tag.hex() + "\n")
+    # Asking user if the key should be saved in the current directory.
+    save_key = input(
+        "Write the key to a file in your current directory? Y/N\n")
+    if save_key == "Y" or save_key== "y":
+        with open(f'key_{filename}.txt', "w") as f:
+            f.write("key: " + key.hex())
 
     # Writing ciphertext to file.
     with open('enc_' + filename, "wb") as f:
@@ -46,36 +37,37 @@ def encrypt():
 # Decrypt file
 def decrypt():
     filename = input("Type a filename you want to decrypt: \n")
-    key_input = input(
-        "Type the key that was given when the file was encrypted: \n")
-    key = bytes.fromhex(key_input)
-    with open(filename, "rb") as f:
-        ciphertext_file = f.read()
 
-    # Finding nonce and tag located in the file.
+    # Finding nonce and tag from the encrypted file.
     with open(filename, "r") as f:
         # NONCE
         f.seek(0, 2)
         position = f.tell()
         f.seek(position - 64, 0)
         bytes64_32 = f.read(32)
-        print("nonce:", bytes64_32)
         nonce_in_end_of_file = bytes64_32
 
         # TAG
         bytes32_16 = f.read()
         f.seek(position - 32, 0)
         bytes32_16 = f.read()
-        print("tag:", bytes32_16)
         tag_in_end_of_file = bytes32_16
 
         nonce = bytes.fromhex(nonce_in_end_of_file)
 
+    with open(filename, 'r+b') as f:
+        # Move to the position 64 bytes from the end of the file
+        f.seek(-64, 2)
+        # Truncate the file at the current position
+        f.truncate()
 
-    verify_authenticity = input("Verify file's authenticity Y/N\n")
-    if verify_authenticity == "Y" or verify_authenticity == "y":
-        # Reading tag from the encrypted file.
-        tag_file = bytes.fromhex(tag_in_end_of_file)
+    key_input = input(
+        "Type the key that was given when the file was encrypted: \n")
+    key = bytes.fromhex(key_input)
+    with open(filename, "rb") as f:
+        ciphertext_file = f.read()
+
+    tag_file = bytes.fromhex(tag_in_end_of_file)
 
     cipher = AES.new(key, AES.MODE_EAX, nonce=nonce)
     plaindata = cipher.decrypt(ciphertext_file)
