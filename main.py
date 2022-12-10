c39cc17af706bb9e77511e4ce70ca9d54f4a1253
OnniVirtanen <116679314+OnniVirtanen@users.noreply.github.com>
2022-12-10 16:40:47 +0200

    Update main.py
    If user inputs wrong key if will ask for new input. There will be a copy file of the encrypted file so if error happens in decrypting it can be tried again with different key etc. Also added fixes to errors.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 from Crypto.Cipher import AES
 
 # Encrypt file
@@ -6,28 +7,39 @@ def encrypt():
     # Create a random secure key.
     # Ask user to enter the filename to encrypt.
     key = os.urandom(16)
-    filename = input("Enter the filename you want to encrypt: \n")
-    print(f'Encrypting file "{filename}"...')
 
-    # Read bytes of the file.
-    # Encrypt the file's bytes and assign it to a variable ciphertext.
-    # Create a nonce and a tag.
-    with open(filename, "rb") as f:
-        data = f.read()
-    cipher = AES.new(key, AES.MODE_EAX)
-    nonce = cipher.nonce
-    ciphertext, tag = cipher.encrypt_and_digest(data)
+    while True:
+        # Asking the user for key input.
+        filename = input("Enter the filename you want to encrypt: \n")
+        try:
+            # Read bytes of the file.
+            # Encrypt the file's bytes and assign it to a variable ciphertext.
+            # Create a nonce and a tag.
 
+            with open(filename, "rb") as f:
+                data = f.read()
+            cipher = AES.new(key, AES.MODE_EAX)
+            nonce = cipher.nonce
+            ciphertext, tag = cipher.encrypt_and_digest(data)
+        except:
+            print("Key is not in hexadecimal format.")
+            continue
+        else:
+            print(f'Encrypting file "{filename}"...')
+            break
+    
     # User must save the printed key for decryption.
     print("The key is required for decryption. If lost, the data will be irrecoverable.")
     print("key:", key.hex())
 
     # Asking user if the key should be saved in the current directory.
     # If true, creating a file containing the key.
-    save_key = input(
-        "Write the key to a file in your current directory? Y/N\n")
-    if save_key == "Y" or save_key== "y":
-        with open(f'key_{filename}.txt', "w") as f:
+    
+  
+    save_key = input("Write the key to a file in your current directory? Y/N\n")
+
+    if save_key == "Y" or save_key == "y":
+        with open(f'secret_key.txt', "w") as f:
             f.write("key: " + key.hex() + "\n")
 
     # Creating a file that contains the encrypted data.
@@ -39,12 +51,17 @@ def encrypt():
     # Writing tag to end of the file.
     with open('enc_' + filename, "a") as f:
         f.write(tag.hex())
-    
+
     print("The file was successfully encrypted.")
+
 
 # Decrypt file
 def decrypt():
     filename = input("Type a filename you want to decrypt: \n")
+
+    # Make a copy of the encrypted file. Keep the original encrypted file safe if user inputs wrong secret key.
+    shutil.copy(filename, "copy" + filename)
+    filename = "copy" + filename
 
     # Reading nonce and tag from the file that is to be decrypted.
     # Assigning variables to nonce and tag.
@@ -57,7 +74,7 @@ def decrypt():
         nonce = bytes.fromhex(nonce)
 
         # Tag
-        tag= f.read()
+        tag = f.read()
         f.seek(position - 32, 0)
         tag = f.read()
         tag = bytes.fromhex(tag)
@@ -67,10 +84,18 @@ def decrypt():
         f.seek(-64, 2)
         f.truncate()
 
-    # Asking the user for key input.
-    key_input = input("Type the key for decryption: \n")
-    key = bytes.fromhex(key_input)
-    print(f'Decrypting file "{filename}"...')
+    # Ask the user for key if the key is not in hex format.
+    while True:
+        # Asking the user for key input.
+        key_input = input("Type the key for decryption: \n")
+        try:
+            key = bytes.fromhex(key_input)
+            print(f'Decrypting file "{filename}"...')
+        except:
+            print("Key is not in hexadecimal format.")
+            continue
+        else:
+            break
 
     # Reading bytes of the encrypted file and assigning it to a variable.
     with open(filename, "rb") as f:
@@ -81,30 +106,35 @@ def decrypt():
         cipher = AES.new(key, AES.MODE_EAX, nonce=nonce)
         plaindata = cipher.decrypt(ciphertext_file)
     except:
-        print("Error in decryption. Key or nonce faulty.")
+        print("Error in decryption.")
 
     # Veryfying the file's authenticity by using the tag.
     try:
         cipher.verify(tag)
         print("The file is authentic.")
+                # Creating a file from the decrypted data.
+        try:
+            with open("dec_" + filename[8:], "wb") as f:
+                f.write(plaindata)
+            print("The file was successfully decrypted.")
+            
+            # Delete encrypted files since they are no longer needed.
+            os.remove(filename)
+            os.remove(filename[4:])
+        except:
+            print("Error when writing decrypted data to a file.")
     except:
         print("The file's authenticity cannot be verified.")
-
-    # Creating a file from the decrypted data.
-    try:
-        with open("dec_" + filename[4:], "wb") as f:
-            f.write(plaindata)
-        print("The file was successfully decrypted.")
-    except:
-        print("Error when writing decrypted data to a file.")
+        decrypt()
 
 # Introduction to the program.
-print("Welcome to VionCrypt!")
+print("Welcome to VionCrypt! ©Onni Virtanen")
 print("This program is used for file encryption and decryption.")
 print("The program uses AES256 algorithm.")
 
 # Asking the user how the program will be used. It can be used for encryption or decryption.
 user_choice = input("Type in E or D, E=Encrypt, D=Decrypt.\n")
+
 if user_choice == "E" or user_choice == "e":
     encrypt()
 elif user_choice == "D" or user_choice == "d":
